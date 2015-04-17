616d3a4ce254134e089ee317927868ed65999552
Ning Xia <nxiamail@gmail.com>
2015-04-17 01:38:42 -0700

    Completed getElementsByClassName - did this by defining a testThisLevel function within the getElementsClassName function definition - it is this testThisLevel function that performs the recursion 	- this allows the lexical output array to be accessed by the recursion, without having to be defined by the recursing function - document.body was used to obtain an array-like object of the elements - element.childNodes was used to obtain an array-like object of that element's child elements - element.classList was used to obtain a list of that element's class attributes 	- classList has a .contains property to check for if a passed classname(as a string) is present in that list

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -7,4 +7,24 @@
 var getElementsByClassName = function(className
 ){
   // your code here
+  var output = [];
+  //begin with all nodes (since technically, <body> can hold a class attr)
+  var allNodes = document.childNodes;
+
+  var testThisLevel = function(children){
+    for (var i = 0; i < children.length; i++) {
+      var current = children[i];
+      // the undefined check will filter out text nodes and unenumerable properties
+      if (current.classList !== undefined && current.classList.contains(className)){
+          output.push(current);
+      } 
+      //recursive call is enclosed under a limiting if statement
+      if (current.childNodes.length>0){
+        testThisLevel(current.childNodes);
+      }
+    };
+  };
+  
+  testThisLevel(allNodes);
+  return output;
 };
