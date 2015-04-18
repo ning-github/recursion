682680f84260eb181e2a4e7f6addf461d9dbfd37
Ning Xia <nxiamail@gmail.com>
2015-04-17 22:51:45 -0700

    Completed stringifyJSON - the return output is to be a stringified primitive value - by way of if conditions on object type, the function recurses down to the primitive value to offer the string version of that 	- the details along the way are simply for object value formatting, excluding undefined and function properties, and treatment of null

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -5,4 +5,44 @@
 
 var stringifyJSON = function(obj) {
   // your code goes here
+  var result = obj;
+
+  //null values should simply return "null" as string
+  if (obj === null){
+    return "null";
+  }
+
+  if (Array.isArray(obj)){
+    for (var i = 0; i < obj.length; i++) {
+      obj[i] = stringifyJSON(obj[i]); //recurse
+    };
+    result = "["+result+"]";
+  }
+  else if (typeof obj === "object"){ 
+    var contents ="";
+    for(var key in obj){
+      //undefined and function properties don't go through
+      if (obj[key]!==undefined && typeof obj[key]!=="function"){  
+        var item="";
+        item+=stringifyJSON(key)+":";   //recurse
+        item+=stringifyJSON(obj[key]);  //recurse
+        contents+=item+",";
+      }
+    }
+    var edited = contents.substr(0, contents.length-1);
+    result = "{"+edited+"}";
+  }
+  //the actual string-making (has to be a primitive value)
+  else{
+    //so that numbers and bools aren't turned to strings when they are property values
+    if(typeof obj !== "string"){
+      result=""+obj+"";
+    }
+    else{
+      console.log("it ran");
+      result = "\""+obj+"\"";
+    }
+  }
+
+  return result;
 };
